dfdfd28df62d5a5cc48c441b0a234ff7fa96cb9b
HeryRobles <100168911+HeryRobles@users.noreply.github.com>
2022-04-30 14:34:23 -0700

    Creamos una lista de Productos

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnAgregarProducto = new System.Windows.Forms.Button();
+            this.dgvProductos = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAgregarProducto
+            // 
+            this.btnAgregarProducto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregarProducto.Location = new System.Drawing.Point(679, 411);
+            this.btnAgregarProducto.Name = "btnAgregarProducto";
+            this.btnAgregarProducto.Size = new System.Drawing.Size(152, 83);
+            this.btnAgregarProducto.TabIndex = 0;
+            this.btnAgregarProducto.Text = "Agregar Artículo";
+            this.btnAgregarProducto.UseVisualStyleBackColor = true;
+            this.btnAgregarProducto.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dgvProductos
+            // 
+            this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProductos.Location = new System.Drawing.Point(0, 1);
+            this.dgvProductos.Name = "dgvProductos";
+            this.dgvProductos.Size = new System.Drawing.Size(840, 404);
+            this.dgvProductos.TabIndex = 1;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(105, 58);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(39, 9);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(843, 506);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dgvProductos);
+            this.Controls.Add(this.btnAgregarProducto);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAgregarProducto;
+        private System.Windows.Forms.DataGridView dgvProductos;
+        private System.Windows.Forms.DataGridView dataGridView2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,32 @@ namespace Ventas
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Producto memoria = new Producto();
+            memoria.Codigo = "9146";
+            memoria.Descripcion = "Memoria Ram";
+            memoria.PrecioUnitario = 199.99m;
+
+            Producto discoDuro = new Producto()
+            {
+                Codigo = "003",
+                Descripcion = "Disco Duro 1T",
+                PrecioUnitario = 520.99m
+            };
+
+            Producto laptop = new Producto()
+            {
+                Codigo = "004",
+                Descripcion = "Laptop Asus",
+                PrecioUnitario = 10000.99m
+            };
+
+            List<Producto> productos = new List<Producto>();
+            productos.Add(memoria);
+            productos.Add(discoDuro);
+            productos.Add(laptop);
+        }
     }
 }
